7ebd0702b809001041977382635ae4b5223861f7
Rickazuo <rick3209@gmail.com>
2023-04-05 16:12:51 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,91 +1,76 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+import cloudArrowUp from "../../public/cloudArrowup.svg";
+import completeArchive from "../../public/completeArchive.svg";
+import completeBar from "../../public/completeBar.svg";
+import failedArchive from "../../public/failedArchive.svg";
+import failedBar from "../../public/failedBar.svg";
+import loadingArchive from "../../public/loadingArchive.svg";
+import loadingBar from "../../public/loadingBar.svg";
+import resumeButton from "../../public/resumeButton.svg";
+import stopButton from "../../public/stopButton.svg";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+      <div className={styles.areaImport}>
+        <Image className={styles.buttonInteractive} src={cloudArrowUp} width={48} height={48} />
+        <p className={`${inter.className} ${styles.titleImportArea}`}>
+          Importe seus arquivos
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <p className={`${inter.className} ${styles.subtitleImportArea}`}>
+          Arraste ou clique para fazer upload
+        </p>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+      <div className={styles.archives}>
+        <div className={styles.loadingArchive}>
+          <Image className={styles.buttonInteractive} src={loadingArchive} width={48} height={56} />
+          <div>
+            <div className={styles.headerLoadingArchive}>
+              <p className={`${inter.className} ${styles.nameArchive}`}>
+                Scann_158.pdf
+              </p>
+              <Image className={styles.buttonInteractive} src={stopButton} />
+            </div>
+            <p className={`${inter.className} ${styles.weightArchive}`}>
+              30 MB / 74 MB
+            </p>
+            <Image  src={loadingBar}/>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className={styles.completeArchive}>
+          <Image className={styles.buttonInteractive} src={completeArchive} width={48} height={56} />
+          <div>
+            <div className={styles.headerLoadingArchive}>
+              <p className={`${inter.className} ${styles.nameArchive}`}>
+                README.rm
+              </p>
+            </div>
+            <p className={`${inter.className} ${styles.weightArchive}`}>
+              12 KB
+            </p>
+            <Image  src={completeBar} />
+          </div>
+        </div>
+        <div className={styles.failedArchive}>
+          <Image className={styles.buttonInteractive} src={failedArchive} width={48} height={56} />
+          <div>
+            <div className={styles.headerLoadingArchive}>
+              <p className={`${inter.className} ${styles.nameArchive}`}>
+                picture1.jpeg
+              </p>
+              <Image className={styles.buttonInteractive} src={resumeButton} />
+            </div>
+            <p className={`${inter.className} ${styles.weightArchive}`}>
+              6.3 MB
+            </p>
+            <Image src={failedBar} />
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
